da4011d1f95b3631c35826df234ba9ac896b1e8a
Mohd Asim <mohd.ashim@mobileprogramming.net>
2019-01-01 03:35:52 +0530

    updated server.js

--- server.js
+++ server.js
@@ -5,22 +5,8 @@ const app = require("./app");
 helmet = require("helmet");
 // set the view engine to ejs
 app.set("view engine", "ejs");
+var server              = require('http').createServer(app);
 const port = process.env.PORT || 3702;
-// const server = http.createServer(app);
-// server.listen(port, () => {
-//   console.log(`Server created on ${port}`);
-//   // console.log(process.env);
-// });
-app.use(helmet()); // Add Helmet as a middleware
-var options = {
-  // key: fs.readFileSync( 'server.key' ),
-  // cert: fs.readFileSync( 'server.crt' ),
-  // requestCert: false,
-  // rejectUnauthorized: false
-  key: fs.readFileSync(path.resolve('server.key')),
-  cert: fs.readFileSync(path.resolve('server.crt'))
-};
-https.createServer(options, app)
-.listen(port, function () {
+server.listen(port, function () {
   console.log(`Example app listening on port ${port}! Go to https://localhost:${port}/`)
 })
