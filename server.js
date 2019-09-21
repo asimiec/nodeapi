4f2167317dffe3dfe96adc995aaba60c6d81c9a1
Mohd <asim.iec@gmail.com>
2019-09-21 11:39:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ helmet = require("helmet");
 // set the view engine to ejs
 app.set("view engine", "ejs");
 var server              = require('http').createServer(app);
-const port = process.env.PORT || 3704;
-server.listen('8080', '172.31.24.18',function () {
+const port = process.env.PORT || 3000;
+server.listen(port, '172.31.24.18',function () {
   console.log(`Example app listening on port ${port}! Go to https://localhost:${port}/`)
 })
